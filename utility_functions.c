c4b7ae9b951dd24987389284ef9ac227a7d64ea4
@philgabby <123733116+CHEGEBB@users.noreply.github.com>
2023-10-18 12:43:41 +0300

    Update utility_functions.c

--- utility_functions.c
+++ utility_functions.c
@@ -1,88 +1,87 @@
 #include "main.h"
 
 /**
- * custom_is_printable - Checks if a character is printable
- * @c: The character to be checked.
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
-int custom_is_printable(char c)
+int is_printable(char c)
 {
-    if (c >= 32 && c < 127)
-        return (1);
+	if (c >= 32 && c < 127)
+		return (1);
 
-    return (0);
+	return (0);
 }
 
 /**
- * custom_append_hexa_code - Appends ASCII in hexadecimal code to a buffer
- * @buffer: Array of characters.
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASCII code.
- *
+ * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int custom_append_hexa_code(char ascii_code, char buffer[], int i)
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-    char hex_map[] = "0123456789ABCDEF";
-    /* The hexadecimal format code is always 2 digits long */
-    if (ascii_code < 0)
-        ascii_code *= -1;
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
 
-    buffer[i++] = '\\';
-    buffer[i++] = 'x';
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 
-    buffer[i++] = hex_map[ascii_code / 16];
-    buffer[i] = hex_map[ascii_code % 16];
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
 
-    return (3);
+	return (3);
 }
 
 /**
- * custom_is_digit - Checks if a character is a digit
- * @c: The character to be checked.
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
-int custom_is_digit(char c)
+int is_digit(char c)
 {
-    if (c >= '0' && c <= '9')
-        return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
 
-    return (0);
+	return (0);
 }
 
 /**
- * custom_convert_size_number - Casts a number to the specified size
- * @num: Number to be cast.
- * @size: Number indicating the type to cast.
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
  *
  * Return: Casted value of num
  */
-long int custom_convert_size_number(long int num, int size)
+long int convert_size_number(long int num, int size)
 {
-    if (size == S_LONG)
-        return (num);
-    else if (size == S_SHORT)
-        return ((short)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
 
-    return ((int)num);
+	return ((int)num);
 }
 
 /**
- * custom_convert_size_unsgnd - Casts an unsigned number to the specified size
- * @num: Number to be cast.
- * @size: Number indicating the type to cast.
+ * convert_size_unsgnd - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
  *
  * Return: Casted value of num
  */
-unsigned long int custom_convert_size_unsgnd(unsigned long int num, int size)
+long int convert_size_unsgnd(unsigned long int num, int size)
 {
-    if (size == S_LONG)
-        return (num);
-    else if (size == S_SHORT)
-        return ((unsigned short)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
 
-    return ((unsigned int)num);
+	return ((unsigned int)num);
 }
-
+/*Collaborators: Brian & Widad */
